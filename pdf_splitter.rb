9a66b293e8f210853905b171e7586325f01041df
Isaac Hitt <isaachitt@Isaacs-MacBook-Pro.local>
2015-05-22 10:40:53 -0500

    Basically working version
    Just need to make page specification work

--- pdf_splitter.rb
+++ pdf_splitter.rb
@@ -3,6 +3,13 @@ require 'docsplit'
 # Pass page number, and it splits that page into a seperate pdf file
 class PdfSplitter
   def pdf_split(file, page_number)
+    Docsplit.extract_pages(file, :pages => 2..3)
+  end
+end
+
+test_splitter = PdfSplitter.new
+
+test_splitter.pdf_split('multi_pdf.pdf', 1)
 
 
 
