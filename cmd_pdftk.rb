3164b19e02b5c231441d2c53e805abea6569a6bc
Ben Granberry <ben@Bens-MacBook-Air.local>
2015-05-22 13:53:20 -0500

    Changed the naming pattern for output files

--- cmd_pdftk.rb
+++ cmd_pdftk.rb
@@ -1,5 +1,10 @@
-in_file = 'multi_pdf.pdf'
-range = 1..2
-range_s = range.to_s.gsub('..', '-')
-`pdftk #{in_file} cat #{range_s} output pages#{range_s}.pdf`
-# res = `cmd`.chomp
+
+class PdfSplitter
+    def split (in_file, range)
+        raw_file_name = /^\w+/.match(in_file)
+
+        `pdftk #{in_file} cat #{range} output #{raw_file_name}_pages#{range}.pdf`
+    end
+end
+
+splitter = PdfSplitter.new.split("multi_pdf.pdf", "2-5")
